2a10e23bd04926bcf0fa35b2a85e74d3c1cf8855
= <=>
2018-10-11 22:00:13 +0700

    update

---
+++ baitapluyen/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using baitapluyen.emtity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace baitapluyen
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Member currentMember;
+
+        public MainPage()
+        {
+            this.currentMember = new Member();
+            this.InitializeComponent();
+            this.NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private async void Click_Save(object sender, RoutedEventArgs e)
+        {
+            this.currentMember.userName = this.Username.Text;
+            this.currentMember.email = this.Email.Text;
+            this.currentMember.phone = this.Phone.Text;
+
+            string jsonMember = JsonConvert.SerializeObject(this.currentMember);
+
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            savePicker.SuggestedFileName = "New Document";
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            await FileIO.WriteTextAsync(file, jsonMember);
+        }
+
+        private async void Click_Load(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            openPicker.FileTypeFilter.Add(".txt");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+
+            string content = await Windows.Storage.FileIO.ReadTextAsync(file);
+            currentMember = JsonConvert.DeserializeObject<Member>(content);
+            Username.Text = currentMember.userName;
+            Email.Text = currentMember.email;
+            Phone.Text = currentMember.phone;
+        }
+    }
+}
---
+++ baitapluyen/emtity/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapluyen.emtity
+{
+    class Member
+    {
+        private string _userName;
+        private string _email;
+        private string _phone;
+
+        public string userName { get => _userName; set => _userName = value; }
+        public string email { get => _email; set => _email = value; }
+        public string phone { get => _phone; set => _phone = value; }
+    }
+}
+
